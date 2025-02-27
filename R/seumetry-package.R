60c4cfa485dc26dbfb85f65e1898266d8d22f338
Malte Borggrewe <me@malteborggrewe.de>
2025-02-27 09:09:48 +0000

    Implement FlowSOM clustering

--- R/seumetry-package.R
+++ R/seumetry-package.R
@@ -4,4 +4,5 @@
 #' @importFrom flowWorkspace flowjo_biexp
 #' @importFrom Biobase AnnotatedDataFrame
 #' @importFrom SingleCellExperiment SingleCellExperiment
+#' @importFrom FlowSOM ReadInput BuildSOM metaClustering_consensus GetClusters GetMetaclusters
 NULL
